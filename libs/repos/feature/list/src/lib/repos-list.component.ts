f1196a9d16e06de9e67d987907206140b1ab24ef
Imad BENSAIF <imad.bensaif@gmail.com>
2021-04-14 14:53:15 +0000

    generating repos list component

---
+++ libs/repos/feature/list/src/lib/repos-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bdev-repos-list',
+  templateUrl: './repos-list.component.html',
+  styleUrls: ['./repos-list.component.scss']
+})
+export class ReposListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/repos/feature/list/src/lib/repos-list.module.ts
+++ libs/repos/feature/list/src/lib/repos-list.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReposListComponent } from './repos-list.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    ReposListComponent
+  ],
 })
 export class ReposListModule {}
