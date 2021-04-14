f492d8b32588d242c362a9e6456218a562b5ad25
Imad BENSAIF <imad.bensaif@gmail.com>
2021-04-14 23:40:42 +0000

    importing ReposListModule to use his component

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { ReposListModule } from '@bdev/repos/feature/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: HomeComponent }
+    ]),
+    ReposListModule
+  ],
   declarations: [
     HomeComponent
   ],
