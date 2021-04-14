485554122c29042b8d2a8517c5e87d9e23ceb14a
Imad BENSAIF <imad.bensaif@gmail.com>
2021-04-14 15:54:48 +0000

    generating home library

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/starred-github-repos',
     '<rootDir>/libs/repos/feature/list',
     '<rootDir>/libs/repos/data-access',
+    '<rootDir>/libs/home',
   ],
 };
--- libs/repos/data-access/src/lib/store/repos-list/repos-list.models.ts
+++ libs/repos/data-access/src/lib/store/repos-list/repos-list.models.ts
@@ -3,4 +3,13 @@
  */
 export interface ReposListEntity {
   id: string | number; // Primary ID
+  name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+  stargazers_count: number;
+  open_issues_count: number;
+  created_at: Date;
 }
