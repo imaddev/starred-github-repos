a94edc4b97827ab9bf1f85a73ab40bb9ffd71b51
Imad BENSAIF <imad.bensaif@gmail.com>
2021-04-14 23:37:52 +0000

    importing ReposDataAccessModule
    to access the repos list store

--- libs/repos/feature/list/src/lib/repos-list.module.ts
+++ libs/repos/feature/list/src/lib/repos-list.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ReposDataAccessModule } from '@bdev/repos/data-access';
 import { ReposListComponent } from './repos-list.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    ReposListComponent
+  imports: [
+    CommonModule,
+    ReposDataAccessModule
   ],
+  declarations: [ReposListComponent],
+  exports: [ReposListComponent]
 })
 export class ReposListModule {}
