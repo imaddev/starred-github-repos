591896973c1bbfe483c4ab748e08de61fafe3f6b
Imad BENSAIF <imad.bensaif@gmail.com>
2021-04-14 23:35:27 +0000

    added loadNextPage effects
    to get results for every page set

--- libs/repos/data-access/src/lib/store/repos-list/repos-list.effects.ts
+++ libs/repos/data-access/src/lib/store/repos-list/repos-list.effects.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { fetch } from '@nrwl/angular';
+import { map } from 'rxjs/operators';
 
 import * as ReposListFeature from './repos-list.reducer';
 import * as ReposListActions from './repos-list.actions';
+import { ReposListService } from '../../../../../feature/list/src/lib/repos-list.service';
 
 @Injectable()
 export class ReposListEffects {
@@ -12,17 +14,37 @@ export class ReposListEffects {
       ofType(ReposListActions.init),
       fetch({
         run: (action) => {
-          // Your custom service 'load' logic goes here. For now just return a success action...
-          return ReposListActions.loadReposListSuccess({ reposList: [] });
+          return this.reposListService.getAll().pipe(
+            map(reposList => ReposListActions.loadReposListSuccess({ reposList }))
+          );
         },
 
         onError: (action, error) => {
           console.error('Error', error);
           return ReposListActions.loadReposListFailure({ error });
-        },
+        }
       })
     )
   );
 
-  constructor(private actions$: Actions) {}
+  loadNextPage$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ReposListActions.setPageNbr),
+      fetch({
+        run: (action) => {
+          return this.reposListService.getAll(action.page).pipe(
+            map(reposList => ReposListActions.loadNextReposListSuccess({ reposList }))
+          );
+        },
+
+        onError: (action, error) => {
+          console.error('Error', error);
+          return ReposListActions.loadReposListFailure({ error });
+        }
+      })
+    )
+  );
+
+  constructor(private actions$: Actions, private reposListService: ReposListService) {
+  }
 }
